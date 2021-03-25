836b4e4e8344887d3c6e423537040243d91f8685
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-03-24 20:49:00 -0600

    Add text to book.js

--- src/book.js
+++ src/book.js
@@ -1,12 +1,51 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return "The " + title;
+};
+
+function buildMainCharacter(name, age, pronouns) {
+  return character = {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  }
+};
+
+var reviewArr = [];
+
+function saveReview(review, reviewArr) {
+  if (reviewArr.includes(review)){
+    console.log("Please add an original review.")
+  } else {
+  return reviewArr.push(review);
 }
+};
+
+function calculatePageCount(title) {
+  return title.length * 20;
+};
+
+//console.log(calculatePageCount("The"));
+
+function writeBook(title, bookCharacter, genre) {
+  return book = {
+    title: title,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(title),
+    genre: genre,
+  }
+};
+
+function editBook(book) {
+  book.pageCount = book.pageCount * .75;
+  return book.pageCount;
+};
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
