32e4cfc5aad83a479befcf76b998a1d7a40e0d13
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-03-27 17:09:41 -0600

    Add code to librarian.js

--- src/librarian.js
+++ src/librarian.js
@@ -1,7 +1,37 @@
 class Librarian {
-  constructor() {
-    
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
   }
-}
+
+  greetPatron(patronName, isMorning) {
+    if (isMorning) {
+      return `Good morning, ${patronName}!`
+    } else {
+      return `Hello, ${patronName}!`
+    }
+  }
+
+  findBook(bookTitle) {
+    var isOnShelf;
+    var userAlert;
+    for (var i = 0; i < library["shelves"]["fantasy"].length; i++) {
+      if (library["shelves"]["fantasy"][i]["title"] === bookTitle) {
+      isOnShelf = true;
+      }
+    }
+    if (isOnShelf) {
+     library["shelves"]["fantasy"].pop();
+     return userAlert = `Yes, we have ${bookTitle}`
+    } else {
+     return  userAlert = `Sorry, we do not have ${bookTitle}`
+    }
+  }
+
+  calculateLateFee(daysLate) {
+    return Math.ceil(daysLate * 0.25)
+  }
+  
+};
 
 module.exports = Librarian;
