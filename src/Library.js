6b00ab1f74e56bc91a53c9e4133471bd2da7409c
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-03-27 15:23:56 -0600

    Add text to Library.js

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,31 @@
+function createLibrary(name) {
+  return library = {
+    name: name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: [],
+    },
+  };
+};
 
+function addBook(library, book) {
+  return library["shelves"][book["genre"]].push(book);
+ };
+
+function checkoutBook(library, bookTitle, genre) {
+  var userAlert = `Sorry, there are currently no copies of ${bookTitle} available at the ${library.name}`
+    for (var i = 0; i < library["shelves"][genre].length; i++) {
+    //console.log(library["shelves"][genre][i]);
+    if (library["shelves"][genre][i]["title"] === bookTitle) {
+      library["shelves"][genre].pop();
+      userAlert = `You have now checked out ${bookTitle} from the ${library.name}` }
+   }
+   return userAlert;
+};
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
